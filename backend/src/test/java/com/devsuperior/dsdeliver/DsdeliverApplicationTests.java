56eb12785d693c7c9eb284c1aeb0a90678681684
daniellegb <daniellegb@protonmail.com>
2021-01-06 18:59:26 -0300

    initial setup

--- backend/src/test/java/com/devsuperior/dsdeliver/DsdeliverApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dsdeliver/DsdeliverApplicationTests.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsdeliver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class DsdeliverApplicationTests {
 
 	@Test
 	void contextLoads() {
+		SpringApplication.run(Dsdeliverapplication.class, args);
 	}
 
 }
