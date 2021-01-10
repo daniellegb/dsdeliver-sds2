4d909c571a77f334e6b7b90a27a3429135f233bc
daniellegb <daniellegb@protonmail.com>
2021-01-10 07:02:44 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -14,23 +14,27 @@ public class OrderDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String address;
-	private double latitude;
-	private double longitude;
+	private Double latitude;
+	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	private Double total;
+	
 	
 	private List<ProductDTO> products = new ArrayList<>();
 	
 	public OrderDTO() {	
 	}
 
-	public OrderDTO(Long id, String address, double latitude, double longitude, Instant moment, OrderStatus status) {
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, 
+			Instant moment, OrderStatus status, Double total) {
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	}
 	
 	public OrderDTO(Order entity) {
@@ -40,6 +44,7 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
+		total = entity.getTotal();
 		products = entity.getProducts().stream().map(x -> new ProductDTO(x)).collect(Collectors.toUnmodifiableList());
 	}
 
@@ -63,7 +68,7 @@ public class OrderDTO implements Serializable {
 		return latitude;
 	}
 
-	public void setLatitude(double latitude) {
+	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
 	}
 
@@ -71,7 +76,7 @@ public class OrderDTO implements Serializable {
 		return longitude;
 	}
 
-	public void setLongitude(double longitude) {
+	public void setLongitude(Double longitude) {
 		this.longitude = longitude;
 	}
 
@@ -95,7 +100,12 @@ public class OrderDTO implements Serializable {
 		return products;
 	}
 
-	
-	
-	
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
 }
