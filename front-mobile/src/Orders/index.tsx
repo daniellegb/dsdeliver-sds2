f7a4c18b88f91780e5c6e68a0ab0db87c6bd3458
daniellegb <daniellegb@protonmail.com>
2021-01-15 19:41:46 -0300

    Add orders list

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Image, StyleSheet, ScrollView, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
+import OrderCard from '../OrderCard';
+
+function Orders() {
+    const handleOnPress =() => {}
+    return (
+        <>
+            <Header />
+            <ScrollView style={styles.container}>
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+            </ScrollView>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        paddingRight: '5%',
+        paddingLeft: '5%',
+    }
+
+});
+
+export default Orders;
---
+++ front-mobile/src/Routes.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator, StackNavigationOptions, HeaderBackground } from "@react-navigation/stack";
+import Home from "./Home";
+import Orders from "./Orders";
+import Header from "./Header";
+
+const Stack = createStackNavigator();
+
+function Routes () {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                headerMode="none"
+                screenOptions={{
+                    cardStyle:{
+                        backgroundColor: '#FFF'
+                    }
+                }}
+            >
+                
+                <Stack.Screen name="Home" component={Home}></Stack.Screen>
+                <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+
+
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    )
+}
+
+export default Routes;
